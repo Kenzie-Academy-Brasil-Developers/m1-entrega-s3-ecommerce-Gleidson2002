674186e02163e661ba38de74f10a4119f03c128f
SkipTags <44418673+Gleidson2002@users.noreply.github.com>
2022-03-19 14:28:45 -0300

    atividade

---
+++ itens.js
@@ -0,0 +1,98 @@
+
+let itens = [
+    {
+    nome: "Acelerador de Plasma Ut-25", 
+    valor: 5300, 
+    id: "Acelerador",
+    categoria: "Armamento",
+    descricao: "Arma de alta eficiência energetica, mira com inteligência artificial com foque automatico. ",
+    imagem: "imagens/armamento/acedeplasma.png"
+   
+},
+{
+    nome: "Regulador térmico feminino",
+    valor: 250,
+    id: "ReguladorFem",
+    categoria: "Trajes",
+    descricao: "Projetado para controlar temperatura corporal em situaçôes estressantes.",
+    imagem: "imagens/Trajes/reguladortermicofeminino.png"
+},
+{
+    nome: "Bombeare Mk-2",
+    valor: 3200,
+    id: "bombardeador",
+    categoria: "Armamento",
+    descricao: "explora o dano fisico contra naves e tanques terrestres. auto poder corrosivo",
+    imagem: "imagens/armamento/bombardeador.png"
+
+},
+{
+    nome: "P.M 3000",
+    valor: 7000,
+    id: "magnetico",
+    categoria: "Armamento",
+    descricao: "desativa qualquer dispositivo por meio de uma onda eletromagnética concentrada",
+    imagem: "imagens/armamento/pulsomagnetico.png"
+    
+},
+{
+    nome: "Capacete de foco retina 50%",
+    valor: 210,
+    id: "retina50",
+    categoria: "Capacete",
+    descricao: "capacete com foco retina 50% versão inicial",
+    imagem: "imagens/capacetes/cfoco.png"
+
+    
+},
+{
+    nome: "Capacete de foco retina 100%",
+    valor: 210,
+    id: "retina100",
+    categoria: "Capacete",
+    descricao: "capacete com foco retina 100% versão final com inteligência artificial e reconhecimento de voz",
+    imagem: "imagens/capacetes/cia.png"
+},
+    
+{
+    nome: "Walkie golden",
+    valor: 120,
+    id: "wgolden",
+    categoria: "Comunicação",
+    descricao: "walkie talkie 250Ghz ",
+    imagem: "imagens/comunicacao/walkie golden.png"
+
+    
+},
+   
+{
+    nome: "Walkie planetary",
+    valor: 120,
+    id: "wplastic",
+    categoria: "Comunicação",
+    descricao: "walkie talkie 500Ghz ",
+    imagem: "imagens/comunicacao/walkie planetary.png"
+
+    
+},
+
+ {
+    nome: "Exo-esqueleto pesado UlSim",
+    valor: 7050,
+    id: "exoesqueleto",
+    categoria: "Trajes",
+    descricao: "Traje multiplicador de força, base 3 mil g*1",
+    imagem: "imagens/Trajes/exoesqueleto.png"
+
+ },
+ {
+    nome: "Regulador térmico Masculino",
+    valor: 250,
+    id: "rtm",
+    categoria: "Trajes",
+    descricao: "Projetado para controlar temperatura corporal em situaçôes estressantes.",
+    imagem: "imagens/Trajes/reguladortermicomasculino.png"
+
+ },
+]
+
---
+++ script.js
@@ -0,0 +1,178 @@
+let body = document.querySelector("body")
+let pagina = "todos"
+const arraycarrinho = [];
+const teste = document.getElementById("todos")
+teste.classList.add("teste")
+let arraytrajes = itens.filter(x => x.categoria === "Trajes")
+let armas = itens.filter(x => x.categoria === "Armamento")
+let capacetes = itens.filter(x => x.categoria === "Capacete")
+let com = itens.filter(x => x.categoria === "Comunicação")
+trajes.addEventListener("click", () => {
+    cardsparaexibir(arraytrajes), catexibindo(trajes)
+})
+todos.addEventListener("click", () => {
+    cardsparaexibir(itens), catexibindo(botodos)
+})
+Armas.addEventListener("click", () => {
+    cardsparaexibir(armas), catexibindo(boarmas)
+})
+cap.addEventListener("click", () => {
+    cardsparaexibir(capacetes), catexibindo(cap)
+})
+comunicacao.addEventListener("click", () => {
+    cardsparaexibir(com), catexibindo(comunicacao)
+})
+
+trajes.addEventListener("click", () => {
+    pagina = "trajes"
+})
+todos.addEventListener("click", () => {
+    pagina = "todos"
+})
+Armas.addEventListener("click", () => {
+    pagina = "armas"
+})
+cap.addEventListener("click", () => {
+    pagina = "cap"
+})
+comunicacao.addEventListener("click", () => {
+    pagina = "com"
+})
+let bpesquisar = document.getElementById("bpesquisa")
+let inputp = document.querySelector("#inputp")
+let botodos = document.getElementById("todos")
+let bocomunicacao = document.getElementById("comunicacao")
+let botrajes = document.getElementById("trajes")
+let bocap = document.getElementById("cap")
+let boarmas = document.getElementById("Armas")
+
+let botoes = [botodos, bocomunicacao, bocap, botrajes, boarmas]
+bpesquisar.addEventListener("click", function () {
+    pesquisa = inputp.value
+
+    cardsparaexibir(pesquisar(pesquisa))
+    inputp.value = ""
+})
+
+function pesquisar(texto) {
+
+    let result = [];
+    for (let i = 0; i < itens.length; i++) {
+
+        if ((itens[i].nome).toLowerCase().includes(texto)) {
+            result.push(itens[i])
+        }
+    }
+    return result
+
+}
+
+function catexibindo(botao) {
+    botao.classList.add("Atual")
+    for (let i = 0; i < botoes.length; i++) {
+        if (botoes[i].classList.contains("Atual")) {
+            botoes[i].classList.remove("Atual")
+
+        }
+    }
+    botao.classList.add("Atual")
+    return
+
+}
+
+function cardsparaexibir(array) {
+    esquerda.innerHTML = ""
+    for (let i = 0; i < array.length; i++) {
+        esquerda.appendChild(criadordecards(array[i]))
+    }
+    return
+}
+
+function criadordecards(objeto) {
+    let produto = document.createElement("div")
+    produto.classList.add("produto")
+    produto.id = objeto.id
+
+    produto.innerHTML = `<div class="produtoicone"><img id="picone" src="${objeto.imagem}" alt=""></div>
+   <p class = classeicone>${objeto.categoria}</p>
+   <p class = nome>${objeto.nome}</p>
+   <p class= descricao>${objeto.descricao}</p>
+   <p class = preco>${objeto.valor}.00 TY </p>
+   <button class = "cadd"  id="${objeto.id}"  >Adicionar ao carrinho</button>`
+
+    return produto
+}
+
+
+function itenscarrinho(objeto) {
+    let carrinho = document.createElement("div");
+    carrinho.classList.add("carrinho");
+    carrinho.id = objeto.id
+    carrinho.innerHTML = `<div class = "carrinhocontent">
+    <div class="carrinhoicone"><img id="cicone" src="${objeto.imagem}" alt=""></div>
+    
+    <div class = "infos">
+    <p class = nome>${objeto.nome}</p>
+    <p class = preco>${objeto.valor}.00 TY </p>
+
+    </div>
+    `
+
+    direita.appendChild(carrinho)
+
+}
+
+let content = document.getElementById("content")
+verificarvazio()
+
+content.addEventListener("click", function (event) {
+    let result = event.target.id
+    for (let i = 0; i < itens.length; i++) {
+        if (itens[i].id === result) {
+            arraycarrinho.push(itens[i])
+            itenscarrinho(itens[i])
+        }
+
+    }
+
+
+    vazioo.innerHTML = ""
+
+    return
+})
+
+
+
+
+
+function verificarvazio() {
+    let vazioo = document.getElementById("vazioo")
+
+    let cvazio = document.createElement("div")
+    let infocarrinho = document.getElementById("infocarrinho")
+
+    if (arraycarrinho.length === 0) {
+        cvazio.innerHTML = `<div class = "vaziovazio"><p id = cv>Carrinho vazio</p><footer id= "adicione">Adicione itens</footer> </div>`
+        vazioo.appendChild(cvazio)
+    } else {
+        infocarrinho.innerHTML = `<div id=carrinfodiv></div>`
+        
+
+    }
+
+    return
+}
+
+function totalcarrinho(array) {
+    let Total = 0
+    for (let i = 0; i < array.length; i++) {
+        Total += array[i].valor
+    }
+    return Total
+
+}
+
+
+cardsparaexibir(itens)
+
+catexibindo(botodos)
